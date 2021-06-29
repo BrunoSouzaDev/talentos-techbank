22fe0f09a4330c32e55b3b6612a59d2cf3109a78
Giovanna Albuquerque <ghb.albuquerque@gmail.com>
2021-06-28 22:00:39 -0300

    first commit

---
+++ src/br/com/techbank/aula0/ClasseMain.java
@@ -0,0 +1,8 @@
+package br.com.techbank.aula0;
+
+public class ClasseMain {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo! Talentos TechBank");
+        System.out.println(System.getProperty("java.version"));
+    }
+}
